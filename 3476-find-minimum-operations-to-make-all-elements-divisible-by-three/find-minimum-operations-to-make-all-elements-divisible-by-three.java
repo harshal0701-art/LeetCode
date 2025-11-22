87357d3678aecc088b866a4ed10148dc837f73d2
Harshal Chaudhari <chaudhariharshal551@gmail.com>
2025-11-22 13:34:12 +0530

    Time: 1 ms (19.52%) | Memory: 43.4 MB (24.42%) - LeetSync

---
+++ 3476-find-minimum-operations-to-make-all-elements-divisible-by-three/find-minimum-operations-to-make-all-elements-divisible-by-three.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+
+        int count = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            
+            if(nums[i] %3 == 0) {
+                continue;
+            }
+
+            if(nums[i] %3 == 1) {
+                nums[i] = nums[i] + 2;
+                count++;
+            } 
+            else {
+                nums[i] = nums[i] + 1;
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
